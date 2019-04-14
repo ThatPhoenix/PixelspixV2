9e0dc2489e9f0a8e37ce97dd2c7b94bbc17b106a
Phoenix <theemaraldblock@gmail.com>
2019-04-14 20:00:31 +0100

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -3,13 +3,12 @@ const Discord = require('discord.js');
 exports.run = function(Pix, message, args) {
   var PixelspixEmbed = new Discord.RichEmbed();
     PixelspixEmbed.setTitle('Pixelspix Credits')
-  PixelspixEmbed.setDescription(`**Pixelspix's Founder:** *Phoenix#8419* \n**Pixelspix's Co-Owner:** *Brands ツ* \n**Pixelspix Head Administrator:** *Elcobrox#3243* /n**Pixelspix Administrator:** *NUT NUT*
-  `);
+  PixelspixEmbed.setDescription(`**Pixelspix's Founder:** *Phoenix#8419* \n**Pixelspix's Co-Owner:** *Brands ツ* \n**Pixelspix Head Administrator:** *Elcobrox#3243* /n**Pixelspix Administrator:** *NUT NUT*`);
   PixelspixEmbed.setColor('#FF000');
   PixelspixEmbed.setImage(`${Pix.user.displayAvatarURL}`)
   PixelspixEmbed.setThumbnail(`${Pix.user.displayAvatarURL}`)
 
-  message.channel.send(AnthonyEmbed);
+  message.channel.send(PixelspixEmbed);
 };
 
 exports.help = {
