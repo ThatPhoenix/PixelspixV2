d23123ee5f3a093c5853a4f55bdadb0616a2c6a6
Phoenix <theemaraldblock@gmail.com>
2019-04-14 20:06:20 +0100

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+exports.run = async(Pix, message, args, tools) => {
+
+      message.channel.send("Yay! Your inviting me!"); 
+    message.channel.send("**Invite URL: \nhttps://discordapp.com/api/oauth2/authorize?client_id=501802312604450820&permissions=8&scope=bot**")
+    message.channel.send("**Support Hub Invite: \nhttps://discord.gg/a4JyMYK**")
+};
