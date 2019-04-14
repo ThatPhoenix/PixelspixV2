c46ff82b1c392cd98b0b4928e8c0d22c2c31ddaf
Phoenix <theemaraldblock@gmail.com>
2019-04-14 21:02:52 +0100

    Update PixelspixSystem.js

--- PixelspixSystem.js
+++ PixelspixSystem.js
@@ -1,6 +1,21 @@
 const Discord = require('discord.js')
 const Pix = new Discord.Client();
 
+Pix.on("message", message => {
+    if (message.author.bot) return;
+    if(message.content.indexOf(settings.prefix) !== 0) return;
+  //Command Loader
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(Pix, message, args);
+    } catch (err) {
+           message.reply(`:x: | Pixelspix Error! **${err}**`)
+        
+    }
+})
+
 
 
 
