efb6f103386559222160fc739ba4385454375014
Phoenix <theemaraldblock@gmail.com>
2019-04-14 20:00:03 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,16 @@
+const Discord = require('discord.js');
 
+exports.run = function(Pix, message, args) {
+  var PixelspixEmbed = new Discord.RichEmbed();
+    PixelspixEmbed.setTitle('Pixelspix Help')
+  PixelspixEmbed.setDescription(`Under developement`)
+  PixelspixEmbed.setColor('#FF000');
+  PixelspixEmbed.setImage(`${Pix.user.displayAvatarURL}`)
+  PixelspixEmbed.setThumbnail(`${Pix.user.displayAvatarURL}`)
+
+  message.channel.send(PixelspixEmbed);
+};
+
+exports.help = {
+name: 'credits'
+};
