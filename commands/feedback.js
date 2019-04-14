22a03ff484f97202c89420a26d4df5fca126655e
Phoenix <theemaraldblock@gmail.com>
2019-04-14 20:14:50 +0100

    Create feedback.js

---
+++ commands/feedback.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (pix, message, args) => {
+    message.delete();
+    if(args[0] == "help"){
+      message.reply("Usage: !feedback <Your feedback here>");
+      return;
+    }
+    let fedback = message.content 
+
+
+    let feedEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor(orange)
+    .addField("Feedback message", `${fedback}`)
+    .addField("Feedback By", `${message.author} with ID: ${message.author.id}`)
+
+    .addField("Time", message.createdAt)
+
+    let reportschannel = Pix.channels.get("567064651423023141").send(feedEmbed)
+    message.channel.send("Your message has been delivered to the Pixelspix V2 Developer Feedback Channel and will be read by one of our admin/developers");
+    reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name: "report"
+}
