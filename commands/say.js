7cb708ddcf565519bc15b514bc3c5a426c060291
Phoenix <theemaraldblock@gmail.com>
2019-05-03 21:54:43 +0100

    Update and rename !say to say.js

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 module.exports.run = async (Pix, message, args) => {
   message.delete();
   let botmessage = args.join(" ");
-  message.channel.send(**`${message.author}':** + botmessage);
+  message.channel.send(**`${message.author.username}':** + botmessage);
 }
 
 module.exports.help = {
