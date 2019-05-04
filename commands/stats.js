0eb4c792d51079b35cf0ee65aae6917160981b87
Phoenix <theemaraldblock@gmail.com>
2019-05-04 11:30:44 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -11,6 +11,7 @@ module.exports.run = async (Pix, message, args) => {
     Pixembed.addField('Uptime in mili-seconds', `${Pix.uptime}`);
     Pixembed.addField('Users :o', `${Pix.users.size}`)
     Pixembed.addField('Channels:', `${Pix.channels.size}`)
+    Pixembed.addField('Servers:', `${Pix.guilds.size`) 
 
     message.channel.send(Pixembed);
 }
