53e28f0ebb3c8a66b4a03a4f90c121ad9f074934
Phoenix <theemaraldblock@gmail.com>
2019-05-04 11:20:34 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,16 +3,16 @@ const Discord = require("discord.js");
 module.exports.run = async (Pix, message, args) => {
     let Pixicon = Pix.user.displayAvatarURL;
     let Pixembed = new Discord.RichEmbed()
-    .setDescription("Pixelspix Stats & Information")
-    .setColor("#15f153")
-    .setThumbnail(Pixicon)
-    .addField("Pixelspix's Name", Pix.user.username)
-    .addField("Pixelspix Was Created On", Pix.user.createdAt);
-    .addField('Uptime in mili-seconds', `${Pix.uptime}`);
-    .addField('Users :o', `${Pix.users.size}`)
-    .addField('Channels:', `${Pix.channels.size}`)
+    Pixembed.setDescription("Pixelspix Stats & Information")
+    Pixembed.setColor("#15f153")
+    Pixembed.setThumbnail(Pixicon)
+    Pixembed.addField("Pixelspix's Name", Pix.user.username)
+    Pixembed.addField("Pixelspix Was Created On", Pix.user.createdAt);
+    Pixembed.addField('Uptime in mili-seconds', `${Pix.uptime}`);
+    Pixembed.addField('Users :o', `${Pix.users.size}`)
+    Pixembed.addField('Channels:', `${Pix.channels.size}`)
 
-    message.channel.send(botembed);
+    message.channel.send(Pixembed);
 }
 
 module.exports.help = {
